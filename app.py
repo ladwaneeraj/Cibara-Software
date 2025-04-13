04a1dc912e411e52ad278a737e2cb6b7b9530de1
Neerajm16 <61719726+ladwaneeraj@users.noreply.github.com>
2025-04-13 17:51:54 +0530

    edit req.txt

--- app.py
+++ app.py
@@ -18,6 +18,7 @@ google_credentials = os.environ.get('GOOGLE_CREDENTIALS')
 credentials_info = json.loads(google_credentials)
 credentials = service_account.Credentials.from_service_account_info(
     credentials_info, scopes=SCOPES)
+
 # Configure logging
 logging.basicConfig(
     level=logging.INFO,
